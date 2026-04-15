528b2f0149428e8c057ac6535c367fa50a62c24f
Kristian Kostov-Hutle <kristian.l.kostov@gmail.com>
2026-04-16 01:58:49 +0200

    random

--- ConsoleApplication1/Prepro.cpp
+++ ConsoleApplication1/Prepro.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <string>
+#include <istream>
+#include <cstdlib>
+#
 using namespace std;
 
-	#define DEBUG_A
+#define DEBUG_A
 #ifdef DEBUG_A
 #define a 30
 #elif DEBUG_B
 #define a 100
-#else 
+#else
 #define a 1000
 #endif
 int main()
 
 {
+	int fieve = 0;
+	std::getline(std::cin, fieve);
 	cout << "Hello c++! \n";
 	cout << a << endl;
 }
