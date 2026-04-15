d7fbff78ce0e9b0d096d26559c7ceb757897451b
Kristian Kostov-Hutle <kristian.l.kostov@gmail.com>
2026-04-16 00:52:39 +0200

    First commit on GitHub

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+	#define DEBUG_A
+#ifdef DEBUG_A
+#define a 10
+#elif DEBUG_B
+#define a 100
+#else 
+#define a 1000
+#endif
+int main()
+
+{
+	cout << "Hello c++! \n";
+	cout << a << endl;
+}
